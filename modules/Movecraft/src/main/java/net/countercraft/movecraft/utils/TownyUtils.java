2e43ad4c2c210a4c9cf07a728b9fee6c99662d04
galacticwarrior9 <zareef1@yahoo.com>
2021-02-10 01:55:37 +0000

    Update Towny API methods and dependency

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/utils/TownyUtils.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/utils/TownyUtils.java
@@ -1,18 +1,12 @@
 package net.countercraft.movecraft.utils;
 
+import com.palmergames.bukkit.towny.TownyUniverse;
 import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import com.palmergames.bukkit.towny.exceptions.TownyException;
-import com.palmergames.bukkit.towny.object.Coord;
-import com.palmergames.bukkit.towny.object.PlayerCache;
+import com.palmergames.bukkit.towny.object.*;
 import com.palmergames.bukkit.towny.object.PlayerCache.TownBlockStatus;
-import com.palmergames.bukkit.towny.object.Resident;
-import com.palmergames.bukkit.towny.object.Town;
-import com.palmergames.bukkit.towny.object.TownBlock;
-import com.palmergames.bukkit.towny.object.TownyPermission;
-import com.palmergames.bukkit.towny.object.TownyUniverse;
-import com.palmergames.bukkit.towny.object.TownyWorld;
 import com.palmergames.bukkit.towny.utils.PlayerCacheUtil;
-import com.palmergames.bukkit.towny.war.flagwar.TownyWarConfig;
+import com.palmergames.bukkit.towny.war.common.WarZoneConfig;
 import net.countercraft.movecraft.Movecraft;
 import net.countercraft.movecraft.config.Settings;
 import net.countercraft.movecraft.localisation.I18nSupport;
@@ -54,7 +48,7 @@ public class TownyUtils {
     public static TownyWorld getTownyWorld(World w) {
         TownyWorld tw;
         try {
-            tw = TownyUniverse.getDataSource().getWorld(w.getName());
+            tw = TownyUniverse.getInstance().getDataSource().getWorld(w.getName());
             if (!tw.isUsingTowny())
                 return null;
         } catch (NotRegisteredException e) {
@@ -107,14 +101,8 @@ public class TownyUtils {
 
     public static boolean validateResident(Player player) {
         Resident resident;
-        try {
-            resident = TownyUniverse.getDataSource().getResident(player.getName());
-            return true;
-        } catch (TownyException e) {
-            //System.out.print("Failed to fetch resident: " + player.getName());
-            //return TownBlockStatus.NOT_REGISTERED;
-        }
-        return false;
+        resident = TownyUniverse.getInstance().getResident(player.getUniqueId());
+        return resident != null;
     }
 
     public static boolean validateCraftMoveEvent(Player player, Location loc, TownyWorld world) {
@@ -133,7 +121,7 @@ public class TownyUtils {
                 bSwitch = true;
             }
         } else {
-            bSwitch = PlayerCacheUtil.getCachePermission(player, loc, type.getId(), data, TownyPermission.ActionType.SWITCH);
+            bSwitch = PlayerCacheUtil.getCachePermission(player, loc, type, TownyPermission.ActionType.SWITCH);
         }
 
 
@@ -144,7 +132,7 @@ public class TownyUtils {
         PlayerCache cache = Movecraft.getInstance().getTownyPlugin().getCache(player);
         TownBlockStatus status = cache.getStatus();
 
-        return !cache.hasBlockErrMsg() && status == TownBlockStatus.WARZONE && TownyWarConfig.isAllowingSwitchesInWarZone();
+        return !cache.hasBlockErrMsg() && status == TownBlockStatus.WARZONE && WarZoneConfig.isAllowingSwitchesInWarZone();
 
     }
 
