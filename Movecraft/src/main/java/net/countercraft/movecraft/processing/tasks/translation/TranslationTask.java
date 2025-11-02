68bbb017d5d1dfc60e18a09f3d51d7cc7e70c429
BuildTools <unconfigured@null.spigotmc.org>
2025-11-02 01:50:53 +0100

    More additions to the new TranslationTask

--- Movecraft/src/main/java/net/countercraft/movecraft/processing/tasks/translation/TranslationTask.java
+++ Movecraft/src/main/java/net/countercraft/movecraft/processing/tasks/translation/TranslationTask.java
@@ -1,7 +1,10 @@
 package net.countercraft.movecraft.processing.tasks.translation;
 
+import com.google.common.collect.Sets;
+import net.countercraft.movecraft.Movecraft;
 import net.countercraft.movecraft.MovecraftLocation;
 import net.countercraft.movecraft.MovecraftRotation;
+import net.countercraft.movecraft.WorldHandler;
 import net.countercraft.movecraft.craft.Craft;
 import net.countercraft.movecraft.craft.SinkingCraft;
 import net.countercraft.movecraft.craft.type.CraftType;
@@ -22,13 +25,16 @@ import net.countercraft.movecraft.processing.tasks.translation.validators.HoverV
 import net.countercraft.movecraft.processing.tasks.translation.validators.MaxHeightValidator;
 import net.countercraft.movecraft.processing.tasks.translation.validators.MinHeightValidator;
 import net.countercraft.movecraft.processing.tasks.translation.validators.WorldBorderValidator;
+import net.countercraft.movecraft.util.MathUtils;
 import net.countercraft.movecraft.util.Tags;
 import net.countercraft.movecraft.util.hitboxes.HitBox;
+import net.countercraft.movecraft.util.hitboxes.MutableHitBox;
 import net.countercraft.movecraft.util.hitboxes.SetHitBox;
+import net.countercraft.movecraft.util.hitboxes.SolidHitBox;
+import net.kyori.adventure.sound.Sound;
 import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.event.Event;
 import org.bukkit.inventory.FurnaceInventory;
 import org.bukkit.inventory.ItemStack;
@@ -36,9 +42,7 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Supplier;
 
 public class TranslationTask implements Supplier<Effect> {
@@ -73,6 +77,9 @@ public class TranslationTask implements Supplier<Effect> {
 
     @Override
     public Effect get() {
+        if (craft.getHitBox().isEmpty()) {
+            return () -> {};
+        }
         var preTranslationResult = preTranslationValidators.stream().reduce(MonadicPredicate::and).orElseThrow().validate(craft);
         if(!preTranslationResult.isSucess()){
             return () -> craft.getAudience().sendMessage(Component.text(preTranslationResult.getMessage()));
@@ -94,6 +101,13 @@ public class TranslationTask implements Supplier<Effect> {
         var phaseLocations = new SetHitBox();
         var harvestLocations = new SetHitBox();
         var fuelSources = new ArrayList<FurnaceInventory>();
+        var passthroughBlocks = new HashSet<>(craft.getType().getMaterialSetProperty(CraftType.PASSTHROUGH_BLOCKS));
+        if (craft instanceof SinkingCraft) {
+            passthroughBlocks.addAll(Tags.FLUID);
+            passthroughBlocks.addAll(Tag.LEAVES.getValues());
+            passthroughBlocks.addAll(Tags.SINKING_PASSTHROUGH);
+
+        }
         for(var originLocation : craft.getHitBox()){
             var originMaterial = craft.getMovecraftWorld().getMaterial(originLocation);
             // Remove air from hitboxes
@@ -152,21 +166,88 @@ public class TranslationTask implements Supplier<Effect> {
 
         // Direct float comparison due to check for statically initialized value
         callCollisionEvent(craft, collisions, preTranslateEvent.getWorld());
-        if(craft.getType().getFloatProperty(CraftType.COLLISION_EXPLOSION) <= 0F && !collisions.isEmpty()){
-            //TODO: collision highlights
-            return () -> craft.getAudience().sendMessage(Component.text(String.format(I18nSupport.getInternationalisedString("Translation - Failed Craft is obstructed") + " @ %d,%d,%d,%s", 0, 0, 0, "not_implemented")));
+        final float collisionExplosion = craft.getType().getFloatProperty(CraftType.COLLISION_EXPLOSION);
+        if(collisionExplosion <= 0F && !collisions.isEmpty()){
+            if (craft instanceof SinkingCraft) {
+                //TODO: collision highlights
+                for (MovecraftLocation collision : collisions) {
+                    Material type = destinationWorld.getMaterial(collision);
+                    if (type.isAir() || !passthroughBlocks.contains(type)) {
+                        continue;
+                    }
+                    destinationLocations.remove(collision);
+                    craft.getCollapsedHitBox().add(collision);
+                }
+
+            } else {
+                MovecraftLocation firstCollision = Collections.min(collisions.asSet(), MovecraftLocation::compareTo);
+                Object obj = craft.getType().getObjectProperty(CraftType.COLLISION_SOUND);
+                if (!(obj instanceof Sound collisionSound)) {
+                    throw new IllegalStateException("COLLISION_SOUND must be of type Sound");
+                }
+                return () -> {
+                    craft.getAudience().playSound(collisionSound);
+                    craft.getAudience().sendMessage(Component.text(String.format(I18nSupport.getInternationalisedString("Translation - Failed Craft is obstructed") + " @ %d,%d,%d,%s",
+                            firstCollision.getX(),
+                            firstCollision.getY(),
+                            firstCollision.getZ(),
+                            destinationWorld.getMaterial(firstCollision))));
+                };
+            }
         }
         Effect fluidBoxEffect = fluidBox(craft, translation);
         var  translateEvent = callTranslateEvent(craft, destinationLocations, preTranslateEvent.getWorld());
+
+
+        final MovecraftWorld originWorld = craft.getMovecraftWorld();
+        final MutableHitBox totalOriginHitBox = new SetHitBox(craft.getHitBox());
+        final MutableHitBox totalDestinationHitBox = new SetHitBox(destinationLocations);
+        if (!passthroughBlocks.isEmpty()) {
+            final var invertedHitBox = Sets.difference(craft.getHitBox().boundingHitBox().asSet(), craft.getHitBox().asSet());
+            Set<Location> overlap = new HashSet<>(craft.getPhaseBlocks().keySet());
+            overlap.removeIf((location -> !craft.getHitBox().contains(MathUtils.bukkit2MovecraftLoc(location))));
+            final int minX = craft.getHitBox().getMinX();
+            final int maxX = craft.getHitBox().getMaxX();
+            final int minY = craft.getHitBox().getMinY();
+            final int maxY = overlap.isEmpty() ? craft.getHitBox().getMaxY() : Collections.max(overlap,
+                    Comparator.comparingInt(Location::getBlockY)).getBlockY();
+            final int minZ = craft.getHitBox().getMinZ();
+            final int maxZ = craft.getHitBox().getMaxZ();
+            final HitBox[] surfaces = {
+                    new SolidHitBox(new MovecraftLocation(minX, minY, minZ), new MovecraftLocation(minX, maxY, maxZ)),
+                    new SolidHitBox(new MovecraftLocation(minX, minY, minZ), new MovecraftLocation(maxX, maxY, minZ)),
+                    new SolidHitBox(new MovecraftLocation(maxX, minY, maxZ), new MovecraftLocation(minX, maxY, maxZ)),
+                    new SolidHitBox(new MovecraftLocation(maxX, minY, maxZ), new MovecraftLocation(maxX, maxY, minZ)),
+                    new SolidHitBox(new MovecraftLocation(minX, minY, minZ), new MovecraftLocation(maxX, minY, maxZ))
+            };
+            final SetHitBox validExterior = new SetHitBox();
+            for (HitBox surface : surfaces) {
+                validExterior.addAll(Sets.difference(surface.asSet(), craft.getHitBox().asSet()));
+            }
+            //Check to see which locations in the from set are actually outside of the craft
+            final Set<MovecraftLocation> confirmed = craft instanceof SinkingCraft
+                    ? invertedHitBox.copyInto(new LinkedHashSet<>())
+                    : verifyExterior(invertedHitBox, validExterior);
+            final Set<MovecraftLocation> failed = Sets.difference(invertedHitBox, confirmed);
+            failed.forEach((moveLoc) -> {
+                totalOriginHitBox.add(moveLoc);
+                totalDestinationHitBox.add(moveLoc.add(translation));
+            });
+
+        }
         //TODO: Sinking?
         //TODO: Collision explosion
         //TODO: phase blocks
-        Effect movementEffect = moveCraft(craft, destinationLocations);
+        Effect phaseEffect = phaseBlocks(craft, destinationWorld, totalOriginHitBox, totalDestinationHitBox, passthroughBlocks);
+        Effect movementEffect = moveCraft(craft, translation, destinationLocations, destinationWorld);
+        Effect unphaseEffect = unphaseBlocks(craft, Objects.requireNonNull(Bukkit.getWorld(originWorld.getWorldUUID())), totalOriginHitBox, totalDestinationHitBox, passthroughBlocks);
         //TODO: un-phase blocks
         Effect teleportEffect = new TeleportationEffect(craft, translation, translateEvent.getWorld());
         return fuelBurnEffect
                 .andThen(fluidBoxEffect)
+                .andThen(phaseEffect)
                 .andThen(movementEffect)
+                .andThen(unphaseEffect)
                 .andThen(teleportEffect);
     }
 
@@ -176,9 +257,16 @@ public class TranslationTask implements Supplier<Effect> {
         return event;
     }
 
-    private static @NotNull Effect moveCraft(Craft craft, HitBox destinationLocations){
+    private static @NotNull Effect moveCraft(Craft craft, MovecraftLocation translation, HitBox destinationLocations, MovecraftWorld destinationWorld){
+        final WorldHandler handler = Movecraft.getInstance().getWorldHandler();
+        final World bukkitWorld = Bukkit.getWorld(destinationWorld.getWorldUUID());
+        if (bukkitWorld == null) {
+            throw new UnsupportedOperationException("Destination world " + destinationWorld.getName() + " is null");
+        }
+        craft.setHitBox(destinationLocations);
         return () -> {
-
+            handler.translateCraft(craft, translation, bukkitWorld);
+            craft.setWorld(bukkitWorld);
         };
     }
 
@@ -243,5 +331,71 @@ public class TranslationTask implements Supplier<Effect> {
 
         double fuelBurnRate = (double) craft.getType().getPerWorldProperty(CraftType.PER_WORLD_FUEL_BURN_RATE, craft.getWorld());
         return submitEvent(new FuelBurnEvent(craft, (double) fuelTypes.get(burningFuel.getType()), fuelBurnRate));
+    }
+
+    private static @NotNull Effect phaseBlocks(@NotNull Craft craft, @NotNull MovecraftWorld destinationWorld, @NotNull HitBox originLocations, @NotNull HitBox destinationLocations, @NotNull Set<Material> passthroughBlocks) {
+        if (passthroughBlocks.isEmpty())
+            return () -> { };
+        return () -> {
+            for (MovecraftLocation moveLoc : destinationLocations) {
+                if (craft.getMovecraftWorld() == destinationWorld && originLocations.contains(moveLoc))
+                    continue;
+                Location bukkitLoc = moveLoc.toBukkit(Bukkit.getWorld(destinationWorld.getWorldUUID()));
+                final BlockData data = bukkitLoc.getBlock().getBlockData();
+                if (!passthroughBlocks.contains(data.getMaterial())) {
+                    continue;
+                }
+                if (craft.getPhaseBlocks().containsKey(bukkitLoc)) {
+                    continue;
+                }
+                craft.getPhaseBlocks().put(bukkitLoc, data);
+            }
+        };
+    }
+
+    private static @NotNull Effect unphaseBlocks(@NotNull Craft craft, @NotNull World oldWorld,  @NotNull HitBox originHitBox, @NotNull HitBox destinationHitBox, @NotNull Set<Material> passthroughBlocks) {
+        if (passthroughBlocks.isEmpty())
+            return () -> { };
+        WorldHandler handler = Movecraft.getInstance().getWorldHandler();
+        return () -> {
+            for (MovecraftLocation origLoc : originHitBox) {
+                final Location bukkit = origLoc.toBukkit(oldWorld);
+                if (!destinationHitBox.contains(origLoc) && craft.getPhaseBlocks().containsKey(bukkit)) {
+                    BlockData data = craft.getPhaseBlocks().remove(bukkit);
+                    handler.setBlockFast(bukkit, data);
+                }
+            }
+        };
+    }
+
+    @NotNull
+    private static Set<MovecraftLocation> verifyExterior(Set<MovecraftLocation> invertedHitBox, SetHitBox validExterior) {
+        var shifts = new MovecraftLocation[]{new MovecraftLocation(0,-1,0),
+                new MovecraftLocation(1,0,0),
+                new MovecraftLocation(-1,0,0),
+                new MovecraftLocation(0,0,1),
+                new MovecraftLocation(0,0,-1)};
+        Set<MovecraftLocation> visited = new LinkedHashSet<>(validExterior.asSet());
+        Queue<MovecraftLocation> queue = new ArrayDeque<>();
+        for(var node : validExterior){
+            //If the node is already a valid member of the exterior of the HitBox, continued search is unitary.
+            for(var shift : shifts){
+                var shifted = node.add(shift);
+                if(invertedHitBox.contains(shifted) && visited.add(shifted)){
+                    queue.add(shifted);
+                }
+            }
+        }
+        while (!queue.isEmpty()) {
+            var node = queue.poll();
+            //If the node is already a valid member of the exterior of the HitBox, continued search is unitary.
+            for(var shift : shifts){
+                var shifted = node.add(shift);
+                if(invertedHitBox.contains(shifted) && visited.add(shifted)){
+                    queue.add(shifted);
+                }
+            }
+        }
+        return visited;
     }
 }
